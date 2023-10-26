0e85be5e999c1ef1cc49d7ae8c8df0f7af0a2d2b
Ronak <ronakasnani5@gmail.com>
2023-10-26 06:58:35 +0530

    completed mail function

--- app.js
+++ app.js
@@ -1,76 +1,78 @@
-require('dotenv').config()
+require("dotenv").config();
 
 // routes.js
-const express = require('express')
-const path = require('path')
-const nodemailer = require('nodemailer')
+const express = require("express");
+const path = require("path");
+const nodemailer = require("nodemailer");
 
-const PORT = process.env.PORT || 5000
-const app = express()
-const router = require('express').Router()
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+const PORT = process.env.PORT || 5000;
+const app = express();
+const router = require("express").Router();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/check-mail-service', (req, res, next) => {
-    return res.status(200).json({ Message: 'Mail Service Working' })
-})
+app.get("/check-mail-service", (req, res, next) => {
+  return res.status(200).json({ Message: "Mail Service Working" });
+});
 
-app.post('/mail', (req, res, next) => {
-    console.log("mail sending ...")
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.TO_EMAIL,
-            pass: process.env.TO_PASSWORD
-        }
-    });
+app.post("/mail", (req, res, next) => {
+  console.log("mail sending ...");
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    // port: 465,
+    // secure: true,
+    auth: {
+      user: process.env.TO_EMAIL,
+      pass: process.env.TO_PASSWORD,
+    },
+  });
 
-    //make mailable object
-    // const mail = {
-    //     from: process.env.SMTP_FROM_EMAIL,
-    //     to: process.env.TO_EMAIL,
-    //     subject: 'New Contact Form Submission',
-    //     text: `
-    //     from:
-    //     ${req.body.name}
+  //make mailable object
+  // const mail = {
+  //     from: process.env.SMTP_FROM_EMAIL,
+  //     to: process.env.TO_EMAIL,
+  //     subject: 'New Contact Form Submission',
+  //     text: `
+  //     from:
+  //     ${req.body.name}
 
-    //     contact details
-    //     email: ${req.body.email}
-    //     phone: ${req.body.tel}
+  //     contact details
+  //     email: ${req.body.email}
+  //     phone: ${req.body.tel}
 
-    //     message:
-    //     ${req.body.message}`,
-    // }
+  //     message:
+  //     ${req.body.message}`,
+  // }
 
-    var mail = {
-        from: req.body.email,
-        to: process.env.TO_EMAIL,
-        subject: req.body.subject,
-        text: req.body.message
-    };
-    
-    transporter.sendMail(mail, (err, data) => {
-        if (err) {            
-            console.log("error => ", err);
-            return res.status(500).json({status: 'fail', message: err})
-        } else {
-            console.log("data => ", data);
-            return res.status(200).json({status: 'success', message: data})
-        }
-    })
+  var mail = {
+    from: req.body.email,
+    to: process.env.TO_EMAIL,
+    subject: `${req.body.subject} from ${req.body.email}`,
+    text: req.body.message,
+  };
 
-    console.log('send => ', mail)
-    return res.status(200).json({ Message: mail })
-})
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      console.log("error => ", err);
+      return res.status(500).json({ status: "fail", message: err });
+    } else {
+      console.log("data => ", data);
+      return res.status(200).json({ status: "success", message: data });
+      //response.sendFile(path.resolve(__dirname, "public", "index.html"));
+    }
+  });
 
-app.use('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, "public", 'index.html'))
-})
+  console.log("send => ", mail);
+  //   return res.status(200).json({ Message: mail });
+  return res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+app.use("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, function () {
-    console.error(`listening on port ${PORT}`)
-})
+  console.error(`listening on port ${PORT}`);
+});
--- public/script.js
+++ public/script.js
@@ -1,13 +1,29 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+
+  const submitButton = document.getElementById("send-mail");
+  const myForm = document.getElementById("mail-form");
+
+  submitButton.addEventListener("click", () => {
+    const name = document.getElementById("name-form").value;
+    const mail = document.getElementById("mail-form").value;
+    const text = document.getElementById("text-form").value;
+    const subject = document.getElementById("subject-form").value;
+    if (name == "" || mail == "" || text == "" || subject == "") {
+      alert("please fill the form");
+      return;
+    }
+    myForm.submit(); // Programmatically submit the form
+    alert("submission successful");
+  });
 })();
